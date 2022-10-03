84a41061ee38ee6eb1535b6d67d73f2b54d7a3e0
Gui-dev <adrianon10@gmail.com>
2022-10-03 03:50:19 -0300

    feat: add animations javascript

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-undef */
+const burger = document.querySelector('nav svg')
+const links = document.querySelectorAll('.links a')
+const videos = gsap.utils.toArray('.video')
+
+burger.addEventListener('click', () => {
+  if (burger.classList.contains('active')) {
+    gsap.to('.links', { x: '100%' })
+    gsap.to('.line', { stroke: 'white' })
+    gsap.set('body', { overflow: 'auto' })
+    gsap.set('body', { overflowX: 'hidden' })
+  } else {
+    gsap.to('.links', { opacity: 1 })
+    gsap.to('.links', { x: '0%' })
+    gsap.to('.line', { stroke: 'black' })
+    gsap.fromTo('.links a', { opacity: 0, y: 0 }, { opacity: 1, y: 20, delay: 0.5, stagger: 0.25 })
+    gsap.set('body', { overflow: 'hidden' })
+  }
+  burger.classList.toggle('active')
+})
+
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    console.log('Clicou')
+    burger.classList.remove('active')
+    gsap.to('.links', { opacity: 0 })
+    gsap.to('.line', { stroke: 'white' })
+    gsap.set('body', { overflow: 'auto' })
+    gsap.set('body', { overflowX: 'hidden' })
+  })
+})
+
+gsap.set(videos, { opacity: 0 })
+
+videos.forEach(video => {
+  ScrollTrigger.create({
+    trigger: video,
+    start: 'top center',
+    end: 'bottom center',
+    onEnter: () => {
+      gsap.to(video, { opacity: 1 })
+      video.play()
+    },
+    onEnterBack: () => video.play(),
+    onLeave: () => video.pause(),
+    onLeaveBack: () => video.pause()
+  })
+})
